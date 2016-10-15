05ba0ed179a72476123e10898f14f0ec23b46f49
Jeff Lawlis <jeff.lawlis@yahoo.com>
2016-10-15 14:49:27 -0500

    working on click function

--- main.js
+++ main.js
@@ -16,8 +16,34 @@ var names = albums.map(function(album, i, arr) {
 });
 
 var divCreate = albums.forEach(function(album, i, arr){
-  var imageDiv = $('<div class="image-div"><img src="'+coverImage[i]+'"/><h2>'+names[i]+'</h2></div>');
+  var imageDiv = $('<div class="image-div'+[i]+'""><img src="'+coverImage[i]+'"/><h2>'+names[i]+'</h2></div>');
   return container.append(imageDiv);
 });
-
+console.dir(document.querySelector('.image-div'));
 // console.log(albums[0].images[0]);
+var albumHeader = $('<h1>My Albums</h1>');
+  $('header').append(albumHeader);
+
+// console.log($('.image-div'));
+
+
+
+$('.image-div0').on('click', function(e) {
+  var album1Photos = albums.filter(function(album, i, arr) {
+    if(album.albumName === 'Album 1')
+    return true;
+  }).map(function(album, i, arr) {
+    return album.images;
+  });
+  // var photoImage = albums.map(function(album, i, arr) {
+  //   // console.log(album.images[i].url);
+  //   return album.images[i].url;
+  // });
+  var photoDiv = albums.forEach(function (album, i, arr) {
+    var imageDiv = $('<div class="image-div1"><img src="'+album1Photos[i]+'"/><h2>'+names[i]+'</h2></div>');
+    return container.html(imageDiv);
+  });
+
+});
+//when image-div is clicked clear container contents
+//run divCreate using images.url to grab album images
